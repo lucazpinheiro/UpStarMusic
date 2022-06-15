cbaf72c3aa8e0d4d7950938b7ed4f2511fd57abd
Lucas <lucascpinheiro10@gmail.com>
2022-06-14 22:19:12 -0300

    add basic crud quueries

--- database/queries/CreateArtist.js
+++ database/queries/CreateArtist.js
@@ -6,4 +6,11 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the Artist that was created
  */
 module.exports = (artistProps) => {
+  const newArtist = new Artist({
+    name: artistProps.name,
+    age: artistProps.age,
+    yearsActive: artistProps.yearsActive,
+    genre: artistProps.genre,
+  });
+  return newArtist.save();
 };
